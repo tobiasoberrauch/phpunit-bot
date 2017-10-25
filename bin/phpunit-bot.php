6b385bf7d4eccc55b509d5175390eeea15d1b324
Tobias Oberrauch <t.oberrauch@simplicity.ag>
2017-10-25 16:11:19 +0200

    Update autoload path

--- bin/phpunit-bot.php
+++ bin/phpunit-bot.php
@@ -6,17 +6,26 @@ use Zend\Console\Console;
 use ZF\Console\Application;
 use ZF\Console\Dispatcher;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+];
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+        break;
+    }
+}
 
 define('VERSION', 0.1);
 
-$botConfig = new BotConfig(include __DIR__ . '/../config/config.php');
+$botConfig = new BotConfig(include getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
 
 $dispatcher = new Dispatcher();
 $dispatcher->map('create', new CreateFromSourceCommand($botConfig));
 
 $application = new Application(
-    'Builder', VERSION, include __DIR__ . '/../config/routes.php', Console::getInstance(), $dispatcher
+    'Builder', VERSION, include getcwd() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'routes.php', Console::getInstance(), $dispatcher
 );
 $exit = $application->run();
 exit($exit);
